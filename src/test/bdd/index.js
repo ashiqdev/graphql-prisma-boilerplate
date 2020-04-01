c405fc4134d8672d8ea091fe5535660bdc522534
Ashiqur Rahman <ashiquejava@gmail.com>
2020-04-01 11:24:19 +0600

    feat: added another test case

--- src/test/bdd/index.js
+++ src/test/bdd/index.js
@@ -10,16 +10,18 @@ dotenv.config({ path: envPath });
 
 chai.should();
 
-
 const { signin } = require("../../resolvers/user/mutation");
 const {
   prisma: { deleteManyUsers }
 } = require("../../../src/generated/prisma-client");
 
-
 // multiply array items by 2
 const multiplyBy2 = numbers => numbers.map(number => number * 2);
-const result = multiplyBy2([1,2,3,4,5]);
+const result = multiplyBy2([1, 2, 3, 4, 5]);
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((a, c) => a + c);
 
 describe("Test Authentication", () => {
   it("should sign in the user", () => {
@@ -34,11 +36,15 @@ describe("Test Authentication", () => {
   it("should be an array", () => {
     expect(["1", "2", "3", "4", "5"])
       .to.be.an("array")
-      .that.includes('2');
+      .that.includes("2");
   });
 
   it("should multiply array items by 2", () => {
     expect(result).to.eql([2, 4, 6, 8, 10]);
+  });
+
+  it("should return the sum of all items of the array", () => {
+    expect(sum).to.equal(15);
   });
 
   after(async () => {
